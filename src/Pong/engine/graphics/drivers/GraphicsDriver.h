5510ee03fd6fac99746e69c2efd48a03e863ab27
alansouls <maiaalan1111@gmail.com>
2025-05-24 01:40:23 +0100

    feat: add zig build

--- src/Pong/engine/graphics/drivers/GraphicsDriver.h
+++ src/Pong/engine/graphics/drivers/GraphicsDriver.h
@@ -1,70 +1,60 @@
 #pragma once
-#define GLFW_INCLUDE_VULKAN
+#include "../utils/Vertex.h"
 #include <GLFW/glfw3.h>
+#include <array>
+#include <glm/glm.hpp>
 #include <optional>
 #include <vector>
-#include <glm/glm.hpp>
-#include <array>
-#include "../utils/Vertex.h"
 
 struct GraphicsDriverOptions {
-	bool debugModeOn;
-	std::vector<char> defaultVertexShader;
-	std::vector<char> defaultFragmentShader;
-	std::vector<char> circleVertexShader;
-	std::vector<char> circleFragmentShader;
+  bool debugModeOn;
+  std::vector<char> defaultVertexShader;
+  std::vector<char> defaultFragmentShader;
+  std::vector<char> circleVertexShader;
+  std::vector<char> circleFragmentShader;
 };
 
 struct QueueFamilyIndices {
-	std::optional<uint32_t> graphicsFamily;
-	std::optional<uint32_t> presentFamily;
+  std::optional<uint32_t> graphicsFamily;
+  std::optional<uint32_t> presentFamily;
 
-	bool isComplete() {
-		return graphicsFamily.has_value() && presentFamily.has_value();
-	}
+  bool isComplete() {
+    return graphicsFamily.has_value() && presentFamily.has_value();
+  }
 };
 
 struct GraphicsOperation;
-class GraphicsDriver
-{
+class GraphicsDriver {
 public:
-	enum ElementType {
-		Quad,
-		Circle
-	};
+  enum ElementType { Quad, Circle };
 
-	GraphicsDriver(GLFWwindow* window, const GraphicsDriverOptions& options) :
-		m_window(window),
-		m_options(options),
-		m_framebufferResized(false)
-	{
+  GraphicsDriver(GLFWwindow *window, const GraphicsDriverOptions &options)
+      : m_window(window), m_options(options), m_framebufferResized(false) {}
 
-	}
+  virtual void init() = 0;
 
-	virtual void init() = 0;
+  virtual void cleanup() = 0;
 
-	virtual void cleanup() = 0;
+  virtual void
+  drawFrame(const std::vector<GraphicsOperation *> &updateOperations) = 0;
 
-	virtual void drawFrame(const std::vector<GraphicsOperation*>& updateOperations) = 0;
+  virtual void performOperation(GraphicsOperation *operation) = 0;
 
-	virtual void performOperation(GraphicsOperation* operation) = 0;
+  virtual void waitIdle() = 0;
 
-	virtual void waitIdle() = 0;
+  void windowResized() { m_framebufferResized = true; }
 
-	void windowResized() {
-		m_framebufferResized = true;
-	}
-
-	virtual glm::vec2 getWindowSize() const = 0;
+  virtual glm::vec2 getWindowSize() const = 0;
 
 protected:
-	GLFWwindow* m_window;
-	GraphicsDriverOptions m_options;
-	bool m_framebufferResized;
+  GLFWwindow *m_window;
+  GraphicsDriverOptions m_options;
+  bool m_framebufferResized;
 
-	static void framebufferResizeCallback(GLFWwindow* window, int width, int height) {
-		auto driver = reinterpret_cast<GraphicsDriver*>(glfwGetWindowUserPointer(window));
-		driver->windowResized();
-	}
+  static void framebufferResizeCallback(GLFWwindow *window, int width,
+                                        int height) {
+    auto driver =
+        reinterpret_cast<GraphicsDriver *>(glfwGetWindowUserPointer(window));
+    driver->windowResized();
+  }
 };
-
