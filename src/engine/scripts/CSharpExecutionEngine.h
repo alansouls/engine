9726a19b6095e01559543e38db0209a77ac99547
Alan Maia <maiaalan1111@gmail.com>
2025-07-29 00:38:17 +0100

    improve CSharpExecutionEngine

--- src/engine/scripts/CSharpExecutionEngine.h
+++ src/engine/scripts/CSharpExecutionEngine.h
@@ -1,23 +1,29 @@
 ﻿#pragma once
 
 #include "ScriptExecutionEngine.h"
+#include "coreclr_delegates.h"
+
+#include <map>
 
 namespace SSGE
 {
 class CSharpExecutionEngine : public ScriptExecutionEngine
 {
   public:
-    CSharpExecutionEngine(std::filesystem::path dotnetProjectPath);
+    CSharpExecutionEngine(std::string projectName, std::filesystem::path dotnetProjectPath);
     ~CSharpExecutionEngine() override = default;
 
     auto init() -> void override;
 
     auto compile() -> bool override;
 
-    auto execute() -> void override;
+    auto execute(const std::string &entryPointClass, const std::string &entryPointMethod) -> void override;
 
-private:
+  private:
+    std::string m_projectName;
     std::filesystem::path m_dotnetProjectPath;
+    load_assembly_and_get_function_pointer_fn m_loadAndGetFunctionPointer;
+    std::map<std::string, component_entry_point_fn> m_componentEntryPoints;
 };
 
 static std::unique_ptr<CSharpExecutionEngine> Instance;
